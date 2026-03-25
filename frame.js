4b53e547b005b307fcd17df8854e08aec7681a0b
samrawit2123 <samrilema418@gmail.com>
2026-03-25 14:53:05 +0300

    Add files via upload

--- frame.js
+++ frame.js
@@ -1,13 +1,11 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // Create frame wrapper
+  
   const frame = document.createElement("div");
   frame.classList.add("js-frame");
 
-  // Move all existing body children into the frame
   while (document.body.firstChild) {
     frame.appendChild(document.body.firstChild);
   }
 
-  // Append the frame to body
   document.body.appendChild(frame);
 });
